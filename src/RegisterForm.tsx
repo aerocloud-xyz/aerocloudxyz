0cf3ef88d0867c4bbf173424b7b2dd13f73eb8df
Antonio0806 <antonio0806123@gmail.com>
2023-09-01 17:00:43 +0200

    Added the register form logic

--- src/RegisterForm.tsx
+++ src/RegisterForm.tsx
@@ -14,9 +14,31 @@ const RegisterForm: React.FC<RegisterFormProps> = () => {
   const [error, setError] = useState<string | null>(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   const [name, setName] = useState("");
+  const isValidEmail = (email: string) => {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return emailPattern.test(email);
+  };
+  const handleRegister = () => {
+    if (!name) {
+        setError('Name is required');
+      }
+      if (!email) {
+        setError('Email is required');
+      } else if (!isValidEmail(email)) {
+        setError('Invalid email format');
+      }
+      if (!password) {
+        setError('Password is required');
+      } else if (password.length < 6) {
+        setError('Password must be at least 6 characters long');
+      } else if (password !== repeatPassword) {
+        setError('The passwords dont match');
+      }
 
-  const handleRegister = () => {};
+    console.log('11');
+  };
   return (
     <div className="register-form-container">
       {error && <p className="error-message">❗ {error}</p>}
@@ -43,6 +65,14 @@ const RegisterForm: React.FC<RegisterFormProps> = () => {
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            fullWidth
+          />
+        </EuiFormRow>
+        <EuiFormRow>
+          <EuiFieldPassword
+            placeholder="Repeat password"
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
             fullWidth
           />
         </EuiFormRow>
