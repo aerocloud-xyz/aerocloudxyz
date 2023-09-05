35cb89f3c11fd62f26208cc94af588d61ffcc8af
Antonio0806 <antonio0806123@gmail.com>
2023-09-05 21:52:58 +0200

    preetified code

--- src/content/UserData.tsx
+++ src/content/UserData.tsx
@@ -1,48 +1,73 @@
-import React from 'react';
-import { EuiText, EuiSplitPanel, EuiTextAlign, EuiButton, EuiAvatar } from '@elastic/eui';
+import React from "react";
+import {
+  EuiText,
+  EuiSplitPanel,
+  EuiTextAlign,
+  EuiButton,
+  EuiAvatar,
+} from "@elastic/eui";
+import TabbedContent from "./TabbedContent";
 
 interface UserDataProps {
-    emailAddress: string;
-    name: string;
-    date: string;
-    handleLogout: () => void;
-    deleteUser: () => void;
-    style: React.CSSProperties | undefined;
-  }
-  const UserData: React.FC<UserDataProps> = ({ emailAddress, name, date, handleLogout, deleteUser, style }) => {
-    return (
-      <EuiSplitPanel.Outer direction="column" style={style}>
-      <EuiSplitPanel.Inner>
-        <EuiAvatar name={name} style={{width: '150px', height: '150px', margin: '25px'}} size={'xl'}/>
-        <EuiText>
-          <p>Email: {emailAddress}</p>
-          <p>Account creation date: {date}</p>
-          <p>Name: {name}</p>
-        </EuiText>
+  emailAddress: string;
+  name: string;
+  date: string;
+  handleLogout: () => void;
+  deleteUser: () => void;
+  style: React.CSSProperties | undefined;
+}
+const UserData: React.FC<UserDataProps> = ({
+  emailAddress,
+  name,
+  date,
+  handleLogout,
+  deleteUser,
+  style,
+}) => {
+  return (
+    <EuiSplitPanel.Outer direction="row">
+      <EuiSplitPanel.Inner grow={false}>
+        <EuiSplitPanel.Outer direction="column" style={style}>
+          <EuiSplitPanel.Inner>
+            <EuiAvatar
+              name={name}
+              style={{ width: "150px", height: "150px", margin: "25px" }}
+              size={"xl"}
+            />
+            <EuiText>
+              <p>Email: {emailAddress}</p>
+              <p>Account creation date: {date}</p>
+              <p>Name: {name}</p>
+            </EuiText>
+          </EuiSplitPanel.Inner>
+          <EuiSplitPanel.Inner grow={false} color="subdued">
+            <EuiTextAlign textAlign="center">
+              <EuiButton
+                color="danger"
+                isDisabled={false}
+                fill
+                onClick={deleteUser}
+                style={{ margin: "10px" }}
+              >
+                Delete account
+              </EuiButton>
+              <EuiButton
+                isDisabled={false}
+                fill
+                onClick={handleLogout}
+                style={{ margin: "10px" }}
+              >
+                Logout
+              </EuiButton>
+            </EuiTextAlign>
+          </EuiSplitPanel.Inner>
+        </EuiSplitPanel.Outer>
       </EuiSplitPanel.Inner>
-      <EuiSplitPanel.Inner grow={false} color="subdued">
-        <EuiTextAlign textAlign='center'>
-          <EuiButton
-              color='danger'
-              isDisabled={false}
-              fill
-              onClick={deleteUser}
-              style={{margin: '10px'}}
-            >
-              Delete account
-            </EuiButton>
-            <EuiButton
-              isDisabled={false}
-              fill
-              onClick={handleLogout}
-              style={{margin: '10px'}}
-            >
-              Logout
-            </EuiButton>
-        </EuiTextAlign> 
+      <EuiSplitPanel.Inner >
+        <TabbedContent />
       </EuiSplitPanel.Inner>
     </EuiSplitPanel.Outer>
-    );
-  };
+  );
+};
 
 export default UserData;
