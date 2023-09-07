3f679991f80bdde99eaa03736869b86cc041448f
Antonio0806 <antonio0806123@gmail.com>
2023-09-07 21:14:45 +0200

    Added some logic regarding name change

--- src/content/UserProfile.tsx
+++ src/content/UserProfile.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { EuiForm, EuiFormRow, EuiButton, EuiFieldText} from '@elastic/eui';
 interface UserProfileProps {
 
 }
 
 const UserProfile: React.FC<UserProfileProps> = () => {
+    const [oldUsername, setOldUsername] = useState('');
+    const [newUsername, setNewUsername] = useState('');
+    useEffect(() => {
+        //TODO: fetch username from authentication server and setOldUsername.
+    });
+    const handleNameChange = () => {
+        //TODO: send username change request to authentication server
+    };
     return (
         <>
-            placeholder
+        <EuiForm>
+        <EuiFormRow>
+          <EuiFieldText
+            placeholder="username"
+            value={oldUsername}
+            onChange={(e) => setNewUsername(e.target.value)}
+            fullWidth
+          />
+        </EuiFormRow>
+        <EuiButton onClick={handleNameChange} fill>
+          Change name
+        </EuiButton>
+      </EuiForm>
         </>
     );
 };
