ff34724c384184c680dd8a0f3202ff334a9fd439
Antonio0806 <antonio0806123@gmail.com>
2023-09-25 22:44:07 +0200

    added token api into constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export const AUTH_API = 'http://localhost:3001/users';
 export const SSH_API = '100.78.61.25';
+//Token verification
+export const TOKEN_API = '';
