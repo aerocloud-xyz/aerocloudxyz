c7c307986eb7938ad0045728fdb5e010182934e3
Antonio0806 <antonio0806123@gmail.com>
2023-09-27 20:03:37 +0200

    cc

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 export const AUTH_API = 'http://localhost:3001/users';
 export const SSH_API = '100.78.61.25';
 //Token verification
-export const TOKEN_API = '';
+export const TOKEN_API = 'http://authentication-edge-sorcerer.santosubito.workers.dev/';
