37340b7c026b8b7aefa38ad160b2988c4d631a72
Antonio0806 <antonio0806123@gmail.com>
2023-09-30 19:51:35 +0200

    added some entries into constants for local dev

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-export const AUTH_API = 'https://aerocloud.xyz:3001/users';
+//export const AUTH_API = 'https://aerocloud.xyz:3001/users';
+//export const API = 'https://aerocloud.xyz:3001/api';
+export const AUTH_API = 'http://localhost:3080/users';
+export const API = 'http://localhost:3080/api';
 export const SSH_API = '2.4.222.204';
 //Token verification
 export const TOKEN_API = 'http://authentication-edge-sorcerer.santosubito.workers.dev/';
