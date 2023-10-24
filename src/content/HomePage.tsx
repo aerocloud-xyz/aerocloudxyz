5c76a59a104c74f1d2c01c49913e5a70a70e0743
Antonio0806 <antonio0806123@gmail.com>
2023-10-24 22:56:32 +0200

    added the homepage stuffs

--- src/content/HomePage.tsx
+++ src/content/HomePage.tsx
@@ -1,7 +1,40 @@
+import {
+  EuiEmptyPrompt,
+  EuiLink,
+  EuiPageTemplate,
+  EuiTitle,
+} from "@elastic/eui";
 import React from "react";
 interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = () => {
-  return (<></>);
+  return (
+    <>
+      <EuiPageTemplate panelled={undefined} offset={0} grow={true}>
+        <EuiPageTemplate.Sidebar sticky={true}></EuiPageTemplate.Sidebar>
+        <EuiEmptyPrompt
+          iconType="cheer"
+          title={<h2>The homepage is coming soon</h2>}
+          color={"subdued"}
+          body={
+            <p>
+              To learn more check the GitHub repository by clicking the link
+              below:{" "}
+            </p>
+          }
+          footer={
+            <>
+              <EuiTitle size="xxs">
+                <h3>Github Repository</h3>
+              </EuiTitle>
+              <EuiLink href="https://github.com/antonio0806/UI" target="_blank">
+                Click here
+              </EuiLink>
+            </>
+          }
+        />
+      </EuiPageTemplate>
+    </>
+  );
 };
 
 export default HomePage;
