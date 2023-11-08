559b4029ef7f01caa77cd6d389db56f956f64992
Antonio0806 <antonio0806123@gmail.com>
2023-11-08 19:37:52 +0100

    bruh slight misclick

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-export const AUTH_API = 'https://aercloud.xyz/auth/users';
+export const AUTH_API = 'https://aerocloud.xyz/auth/user';
 export const API = 'https://aerocloud.xyz/auth/api';
 //export const AUTH_API = 'http://localhost:3080/users';
 //export const API = 'http://localhost:3080/api';
