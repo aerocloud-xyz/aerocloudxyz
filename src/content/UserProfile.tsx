d9442b16d7e0f82cdedc62fed95f726ecb12ed29
Antonio0806 <antonio0806123@gmail.com>
2023-12-02 22:55:02 +0100

    stuff

--- src/content/UserProfile.tsx
+++ src/content/UserProfile.tsx
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { EuiForm, EuiFormRow, EuiButton, EuiFieldText } from "@elastic/eui";
+import { EuiMarkdownEditor, EuiSplitPanel } from "@elastic/eui";
 interface UserProfileProps {}
 
 const UserProfile: React.FC<UserProfileProps> = () => {
-  const [oldUsername, setOldUsername] = useState("");
-  const [newUsername, setNewUsername] = useState("");
-  useEffect(() => {
-    //TODO: fetch username from authentication server and setOldUsername.
-  });
-  const handleNameChange = () => {
-    //TODO: send username change request to authentication server
+  const initialContent4 = `## 👋 Hello again!
+
+I'm just a **EuiMarkdownEditor** with:
+- a \`height\` set to \`200\`
+- a \`maxHeight\` set to \`300\`
+`;
+  const onvalue = () => {
+    console.log("asdf");
   };
+  useEffect(() => {});
   return (
     <>
-      <EuiForm>
-        <EuiFormRow>
-          <EuiFieldText
-            placeholder="username"
-            value={oldUsername}
-            onChange={(e) => setNewUsername(e.target.value)}
-            fullWidth
-          />
-        </EuiFormRow>
-        <EuiButton onClick={handleNameChange} fill>
-          Change name
-        </EuiButton>
-      </EuiForm>
+      <EuiSplitPanel.Outer direction="row">
+        <EuiSplitPanel.Inner style={{height: 500, maxHeight: 1000}}>
+        <EuiMarkdownEditor
+              aria-label="EUI markdown editor with fixed and max height"
+              placeholder="Your markdown here..."
+              initialViewMode="viewing"
+              value={initialContent4}
+              onChange={onvalue}
+              height={400}
+              maxHeight={1000}
+            />
+        </EuiSplitPanel.Inner>
+        <EuiSplitPanel.Inner></EuiSplitPanel.Inner>
+      </EuiSplitPanel.Outer>
     </>
   );
 };
