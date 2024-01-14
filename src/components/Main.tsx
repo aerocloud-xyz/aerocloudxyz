e73e1df3b7de732a3e916cf2b1e63e386474c1a8
Antonio0806 <antonio0806123@gmail.com>
2024-01-14 22:42:43 +0100

    Moved the Home body into a separate file

---
+++ src/components/Main.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import HomePage from "../content/HomePage";
+import UserData from "../content/UserData";
+interface props {}
+const Main: React.FC<props> = () => {
+    const name = '';
+    const email = '';
+    const date = '';
+    const role = '';
+    const handleLogout = () => {}
+    const handleDeletion = () => {}
+    const handleLogin = () => {}
+    const isLoggedIn: boolean = false
+
+  return (
+    <>
+{/*           <UserData
+            emailAddress={email}
+            name={name}
+            date={date}
+            role={role}
+            handleLogout={handleLogout}
+            deleteUser={handleDeletion}
+            handleSwitchToHomePageFromProfile={
+              handleSwitchToHomePageFromProfileBruv
+            }
+            style={{
+              width: "300px",
+              height: "100 %",
+              marginLeft: "25px",
+              marginTop: "15px",
+              textAlign: "center",
+            }}
+          /> */}
+        <HomePage LoggedIn={isLoggedIn} /> 
+    </>
+  );
+};
+
+export default Main;
