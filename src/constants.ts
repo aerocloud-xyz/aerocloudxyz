425a8da9b2182a73a3def2dceab2e4c1c7b13e9c
Antonio0806 <antonio0806123@gmail.com>
2024-01-30 21:40:52 +0100

    Revert "small constants change"
    This reverts commit 4aaee976bb1424704fa47cb2d0e0f8e2d2fcef85.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
-export const AUTH_API = 'https://auth.aerocloud.xyz/auth/users';
-export const API = 'https://auth.aerocloud.xyz/auth/api';
+export const AUTH_API = 'https://aerocloud.xyz/auth/users';
+export const API = 'https://aerocloud.xyz/auth/api';
 //export const AUTH_API = 'http://localhost:3080/users';
 //export const API = 'http://localhost:3080/api';
 export const WEBSSH_URL = 'https://localhost:2224/';
