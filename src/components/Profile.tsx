d180ad4e1276271bb8b614e718841457e755f2dd
Antonio0806 <antonio0806123@gmail.com>
2024-01-31 19:40:08 +0100

    fixed up an error

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import UserData from "../content/UserData";
 import handleLogout from "../utils/logout";
