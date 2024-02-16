99209c4a3dea856483d262e4f21dd37baff4fc5e
Antonio0806 <antonio0806123@gmail.com>
2024-02-16 17:43:17 +0100

    authentication endpoint change

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
-export const AUTH_API = 'https://auth.aerocloud.xyz/auth/users';
-export const API = 'https://auth.aerocloud.xyz/auth/api';
-//export const AUTH_API = 'http://localhost:3080/users';
-//export const API = 'http://localhost:3080/api';
+//export const AUTH_API = 'https://auth.aerocloud.xyz/auth/users';
+//export const API = 'https://auth.aerocloud.xyz/auth/api';
+export const AUTH_API = 'http://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/users';
+export const API = 'http://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/api';
 export const WEBSSH_URL = 'https://localhost:2224/';
 export const AERONOTE_API = 'https://notes.aerocloud.xyz';
