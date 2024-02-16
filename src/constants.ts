9c3f0a8726886bc0d180d602114043a8a9eb003d
Antonio0806 <antonio0806123@gmail.com>
2024-02-16 18:18:02 +0100

    changed http to https

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 //export const AUTH_API = 'https://auth.aerocloud.xyz/auth/users';
 //export const API = 'https://auth.aerocloud.xyz/auth/api';
-export const AUTH_API = 'http://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/users';
-export const API = 'http://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/api';
+export const AUTH_API = 'https://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/users';
+export const API = 'https://aerocloudxyz-authentication--n1k5z7y.kindsmoke-decbc255.germanywestcentral.azurecontainerapps.io/api';
 export const WEBSSH_URL = 'https://localhost:2224/';
 export const AERONOTE_API = 'https://notes.aerocloud.xyz';
