325165fc3da09dca5cc08e0f9e9b53c5bd7efcde
Antonio0806 <antonio0806123@gmail.com>
2024-03-04 23:13:41 +0100

    optimized the notes table display

--- src/pages/noteComponents/notesTable.tsx
+++ src/pages/noteComponents/notesTable.tsx
@@ -6,7 +6,8 @@ import {
   EuiTableFieldDataColumnType,
   EuiLoadingSpinner,
 } from "@elastic/eui";
-type NoteInTable = {
+import { useNavigate } from "react-router-dom";
+type Note = {
   id: string;
   title: string;
   owner: string;
@@ -16,108 +17,80 @@ type NoteInTable = {
   createdDate: string;
   updatedDate: string;
 };
-
-type NoteObject = {
-  content: {
-    name: string;
-    mediaType: string;
-    enablePublicUrl: boolean;
-    signedUrlTimeout: number;
-    uploadUrlTimeout: number;
-    size: number;
-    version: number;
-    url: string;
-  };
+/* type Note = {
   id: string;
-  isPublic: boolean;
-  isShort: boolean;
-  owner: string;
-  xata: {
-    createdAt: string;
-    updatedAt: string;
-    version: number;
-  };
-};
-var notes: NoteInTable[] = [];
-const NotesTable = () => {
+  name: string;
+  dateOfCreation: string | undefined;
+  email: string;
+}; */
+var notes: Note[] = [];
+export default () => {
   const [isShowingLoadingScreen, setIsShowingLoadingScreen] = useState(true);
+  const navigate = useNavigate();
   useEffect(() => {
     notes = [];
     const loadDataIntoTable = () => {
       const userToken = localStorage.getItem("usertoken");
+      const formData = new URLSearchParams();
       if (userToken) {
-        fetch(
-          `https://notes.aerocloud.xyz/api/getusersmsg?token=${userToken}`,
-          {
-            method: "GET",
-          }
-        )
-          .then(async (response) => {
-            if (response.ok) {
-              const responseText = await response.text();
-              const noteArray: NoteObject[] = JSON.parse(responseText);
-              //console.log(userArray);
-              notes = [];
-              for (let i = 0; i < noteArray.length; i++) {
-                const note = noteArray[i]; // Get the user data from the response
-                notes.push({
-                  id: note.id,
-                  owner: note.owner,
-                  title: note.id,
-                  url: note.content.url,
-                  createdDate: note.xata.createdAt,
-                  updatedDate: note.xata.updatedAt,
-                  isPublic: "false",
-                  isShort: "false",
-                });
-              }
-              setIsShowingLoadingScreen(false);
-            } else {
-              //nah
-              console.log("nie zesraj sie!!!");
-              notes = [];
-            }
-          })
-          .catch((error) => {
-            console.error(
-              "There was a problem with the fetch operation:",
-              error
-            );
-          });
+        formData.append("token", userToken);
       } else {
         notes = [];
         console.error("User token is missing or invalid.");
       }
+      fetch(
+        `https://notes.aerocloud.xyz/api/getusersmsg?token=${userToken}`,
+        {
+          method: "GET",
+        }
+      )
+        .then(async (response) => {
+          if (response.ok) {
+            let notesArray = [];
+            const responseText = await response.text();
+            notesArray = JSON.parse(responseText);
+            notes = [];
+            for (let i = 0; i < notesArray.length; i++) {
+              const noteB = notesArray[i]; // Get the note data from the response
+              notes.push({
+                id: noteB.id,
+                owner: noteB.owner,
+                title: noteB.id,
+                url: noteB.content.url,
+                createdDate: noteB.xata.createdAt,
+                updatedDate: noteB.xata.updatedAt,
+                isPublic: "false",
+                isShort: "false",
+              });
+            }
+            setIsShowingLoadingScreen(false);
+          } else {
+            //nah
+            console.log('nie zesraj sie!!!')
+            notes = [];
+          }
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
     };
     loadDataIntoTable();
-  }, []);
-  const columns: Array<EuiBasicTableColumn<NoteInTable>> = [
-    {
-      field: "owner",
-      name: "Owner",
-      "data-test-subj": "nameCell",
-      mobileOptions: {
-        render: (note: NoteInTable) => <span>{note.owner}</span>,
-        header: false,
-        truncateText: false,
-        enlarge: true,
-        width: "100%",
-      },
-    },
+  });
+  const columns: Array<EuiBasicTableColumn<Note>> = [
     {
       field: "title",
-      name: "Title",
-      // render: (title: NoteInTable["title"]) => title,
+      name: "Name",
+      "data-test-subj": "nameCell",
     },
     {
       field: "createdDate",
-      name: "Date of Creation",
-      // render: (note: NoteInTable) => <span>{note.createdDate}</span>,
+      name: "Created at",
+      dataType: "date",
     },
     {
       field: "updatedDate",
-      name: "Date of Update",
-      // render: (note: NoteInTable) => <span>{note.updatedDate}</span>,
+      name: "Updated at",
+      dataType: "date",
     },
     {
       name: 'Actions',
@@ -127,13 +100,23 @@ const NotesTable = () => {
           description: 'Edit this note',
           type: 'icon',
           icon: 'copy',
-          onClick: (note: NoteInTable) => {console.log(note)},
+          onClick: (note: Note) => {
+            navigate(`/aeronote/edit/${note.id}`)
+          },
+        },
+        {
+          name: 'Delete',
+          description: 'Delete this note',
+          type: 'icon',
+          icon: 'trash',
+          color: 'danger',
+          onClick: () => {},
         },
       ],
     },
   ];
-  const getRowProps = (note: NoteInTable) => {
-    const { id } = note;
+  const getRowProps = (user: Note) => {
+    const { id } = user;
     return {
       "data-test-subj": `row-${id}`,
       className: "customRowClass",
@@ -141,10 +124,10 @@ const NotesTable = () => {
     };
   };
   const getCellProps = (
-    note: NoteInTable,
-    column: EuiTableFieldDataColumnType<NoteInTable>
+    user: Note,
+    column: EuiTableFieldDataColumnType<Note>
   ) => {
-    const { id } = note;
+    const { id } = user;
     const { field } = column;
     return {
       className: "customCellClass",
@@ -152,38 +135,32 @@ const NotesTable = () => {
       textOnly: true,
     };
   };
-  return isShowingLoadingScreen ? (
-    //loading
+  return (
+    isShowingLoadingScreen ? (
+      //loading
     <>
-      <div
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <EuiLoadingSpinner
-          size="xxl"
-          style={{ position: "absolute", textAlign: "center" }}
-        />
-      </div>
-    </>
-  ) : (
-    <EuiBasicTable
-      tableCaption="Notes"
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <EuiLoadingSpinner
+        size="xxl"
+        style={{ position: "absolute", textAlign: "center" }}
+      />
+    </div>
+  </>
+    ):(    <EuiBasicTable
+      tableCaption="Users"
       items={notes}
-      rowHeader="Owner"
+      rowHeader="Name"
       columns={columns}
       rowProps={getRowProps}
       cellProps={getCellProps}
-      hasActions={true}
-      style={{
-        marginTop: "50px",
-        marginLeft: "25px",
-        marginRight: "25px",
-      }}
-    />
+    />)
+
   );
 };
-export default NotesTable;
